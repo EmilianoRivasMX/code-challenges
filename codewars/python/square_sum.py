98c13d3c6b364abdbf6f062deb8d99228499b2db
EmilianoRivasMX <emilianorivasxd@gmail.com>
2023-04-04 02:51:38 -0600

    Add codewars python challenge

---
+++ codewars/python/square_sum.py
@@ -0,0 +1,8 @@
+""" Square Sum
+
+Complete the square sum function so that it squares each number passed into it 
+and then sums the results together.
+"""
+
+def square_sum(numbers):
+    return sum([n ** 2 for n in numbers])
